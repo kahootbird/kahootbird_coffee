4c2f5eb7af6b26440df8b8b209977a6e6b2d66a5
Ronnie Goodrich <kahootbird@gmail.com>
2021-10-13 17:35:20 -0400

    added login page

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
+
+const bodyparser = require("body-parser")
+const session = require("express-session")
+app.use(body-parser.json())
+app.use(bodyparser.urlencoded({extended:true}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
