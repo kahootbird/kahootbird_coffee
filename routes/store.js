e2259c866b29ba2d3190d1640a07c4080a70cc20
Ronnie Goodrich <kahootbird@gmail.com>
2021-10-18 23:14:46 -0400

    Listed items for sale in store div. Experimented with id URL.

--- routes/store.js
+++ routes/store.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+//Test getting routate at ID
+/*
+router.get('/id/:id', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+*/
+
 router.get('/', function(req, res, next) {
   res.render('store', { title: 'Express' });
 });
+
+
+
 
 module.exports = router;
