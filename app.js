d14a8748b2600acb0e2f4814f1798ff256c8deae
Ronnie Goodrich <kahootbird@gmail.com>
2021-10-19 21:38:16 -0400

    Changed from pug to ejs.

--- app.js
+++ app.js
@@ -55,7 +55,10 @@ app.use(session({
 */
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
