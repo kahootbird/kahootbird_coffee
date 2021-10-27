0928a45e681595dd673111b5c8a1d5867e2d7511
Ronnie Goodrich <kahootbird@gmail.com>
2021-10-26 23:22:25 -0400

    Got API to reading data in route from object

--- routes/api.js
+++ routes/api.js
@@ -15,24 +15,50 @@ router.get('/', function(req, res, next) {
     console.log("CONNECTED")
     var collection = client.db("test")
       read = collection.collection('employees').find({Employeeid: 2})
-      console.log("read:" + read)
+      //console.log("read:" + read)
       var z = read.length
       var zcount = 0
+      console.log("read" + read)
 
-      read.forEach(item => {
+  Object.size = function(obj) {
+  var size = 0,
+    key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) size++;
+  }
+  //console.log("KEY " + key)
+  return size;
+};
+
+        const myObj = {}
+        var size = Object.size(read);
+        read.forEach(function(item, index)  {
         z = read.length
+        console.log("TPYE " + typeof(item))
         item_array.push(item)
-        //console.log(item_array[0])
-        zcount++
-        console.log (read)
-        if (zcount == z)
-        console.log("END FOREACH")
-        //console.log(item)
-      })
-      console.log("RENDERING ITEM ARRAY: " + item_array[0])
-      res.render('api', { post: req.query.post, author: item_array[0] });
-  });
+        console.log(item_array)
 
+        zcount++
+        //console.log (read)
+        if (zcount == size)
+        {
+          var str = []
+          for(var i in item_array[0]){
+            console.log("DATA")
+            console.log(i)
+            console.log(item_array[0][i])
+            str.push(item_array[0][i])
+          }
+          console.log("ARRAY " + item_array)
+          //render
+          console.log("RENDERING ITEM ARRAY: " + item_array[0])
+          res.render('api', { post: req.query.post, author: str });
+          console.log("END FOREACH")
+        }
+      })
+
+
+  });
 
   //console.log(req.query.fname)
   //res.render('api', { post: req.query.post, author: item_array });
