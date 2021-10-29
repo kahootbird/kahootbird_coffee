36dac1569c8859f1aa7939288a1529c8de396724
Ronnie Goodrich <kahootbird@gmail.com>
2021-10-28 22:34:41 -0400

    Formatted code slightly better

--- routes/api.js
+++ routes/api.js
@@ -11,28 +11,29 @@ var item_array = []
 router.get('/', function(req, res, next) {
 
   client.connect(err => {
-    console.log("TYPE" + req.query.type)
-    item_array = []
     var post_or_get = 0
+    var collection = client.db("test")
+
+    item_array = []
+
     if (req.query.type == 0)
-    post_or_get = 0
+      post_or_get = 0
     else if (req.query.type == 1)
-    post_or_get = 1
+      post_or_get = 1
     else {
       post_or_get = 0
     }
 
-      var collection = client.db("test")
-      read = collection.collection('author').find({Authorid: 1})
+
       //console.log("read:" + read)
-      var z = read.length
+      //var z = read.length
       var zcount = 0
       console.log("read" + read)
 
 console.log("POST OR GET: " + post_or_get)
       if (post_or_get == 0)
       {
-        console.log("added author")
+        //Get data from DB
         collection.collection('author').insertOne({
             Authorid: 1,
             Author: "I am the author",
@@ -41,26 +42,34 @@ console.log("POST OR GET: " + post_or_get)
         res.render('api', { post: "added author", author: "added author" });
       }
       else {
-        console.log("read author")
+  //Read data from DB
+  read = collection.collection('author').find({Authorid: 1})
+  console.log("read author")
+
+  //Get the object size
   Object.size = function(obj) {
   var size = 0,
     key;
   for (key in obj) {
+    //console.log("key is" + key + " STATE IS " + obj.hasOwnProperty(key))
     if (obj.hasOwnProperty(key)) size++;
   }
   return size;
+
 };
 
         const myObj = {}
         var size = Object.size(read);
+        console.log("SIZE TO READ IS:" + size)
 
+        //Use object size to determine data to post
         read.forEach(function(item, index)  {
-        z = read.length
+        //z = read.length
         console.log("type " + typeof(item))
         item_array.push(item)
         console.log(item_array)
         zcount++
-
+        console.log("REAL SIZE:" + zcount)
         if (zcount == size)
         {
           var str = []
